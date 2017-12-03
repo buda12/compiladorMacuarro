36f32a6811f6f1cff8956e94d014dc6f8485bb7c
David <David@LAPTOP-I6T2BCAE>
2017-12-02 19:32:16 -0800

    analizador sintactico y lexico

--- Compilador/Program.cs
+++ Compilador/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Collections;
 using System.Threading.Tasks;
 
 namespace Compilador
@@ -10,6 +12,290 @@ namespace Compilador
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            Console.WriteLine("Introduzca la direccion del archivo: ");
+            String x = Console.ReadLine();
+            Lexico lex = new Lexico(x);
+            Sintactico sin = new Sintactico(lex);
+            sin.imprimir();
+            Console.ReadLine();
+        }//Main
+    }//class Program
+
+    struct token
+    {//tipos son identifier, keyword, operator, special, constant,punctuation, comparison,
+        public string tipo;
+        public string lexema;
+        public int linea;
+        public int index;
+        public token(string t, string l, int line, int i)
+        {
+            tipo = t;
+            lexema = l;
+            linea = line;
+            index = i;
+        }//token
+         //tome strings con todo y colmilla
+    }//token
+
+    class Lexico
+    {
+        string[] keyword = { "entero", "palabra", "real", "caracter" };
+        string[] metodo = { "para", "mientras", "si", "imprimir", "simprimir", "leer", "sino" };
+        string[] oper = { "+", "-", "*", "/", "^", "%", "=" };
+        string[] cmp = { ">", "<", ">=", "<=", "==", "!=" };
+        string[] corch = { "{", "}", "(", ")", "[", "]" };
+        string prog = "";
+        int linea = 0;
+        int index = 0;
+        LinkedList<token> tokens = new LinkedList<token>();
+        LinkedList<string> errors = new LinkedList<string>();
+
+        public Lexico(string lex)
+        {
+            StreamReader obj = new StreamReader(lex);
+            while (prog != null)
+            {
+                prog = obj.ReadLine();
+                linea += 1;
+                if (prog != null)
+                {
+                    index = 0;
+                    control();
+                }//if(prog!=null)
+                else
+                {
+                    obj.Close();
+                    //print();
+                }//else
+            }//while
+             //give it the line of codeprog=lex;
+
+
+
+            //que pare al ver un ; o ,  ... los reconoce como algun tipo de token?
+
+
+        }//Lexico
+        public string read()
+        {//string must be empty first time it comes
+            string wrd = "";
+            //bool first=true;
+            while (index < prog.Length)
+            {
+                if (prog.ElementAt(index) == 32 && wrd != string.Empty)
+                {
+                    return wrd;
+                }//if space and i have something
+                else
+                {
+                    if (prog.ElementAt(index) == 39 || prog.ElementAt(index) == 34)
+                    {
+                        if (wrd == string.Empty)
+                        {
+                            return expressRead();
+                        }//if(prog==string.Empty)
+                        else
+                        {
+                            return wrd;
+                        }//else
+                    }//if(prog.ElementAt(index)=="'"|| prog.ElementAt(index)==34)
+                    if (oper.Contains(prog.ElementAt(index).ToString()) || corch.Contains(prog.ElementAt(index).ToString()) || prog.ElementAt(index) == 59 || prog.ElementAt(index) == 44 || prog.ElementAt(index) == 62 || prog.ElementAt(index) == 60 || prog.ElementAt(index) == 33)
+                    {
+                        if (wrd == string.Empty)
+                        {
+                            if (prog.ElementAt(index) >= 60 && prog.ElementAt(index) <= 62 || prog.ElementAt(index) == 33)
+                            {
+                                wrd = getCompare();
+                                index += 1;
+                                if (wrd.Length > 1) { index += 1; }//2 valores de comparacion
+                            }//first item of a comparison
+                            else
+                            {
+                                wrd += prog.ElementAt(index);
+                                index += 1;
+                            }//no parece ser comparacion
+
+                            return wrd;
+                        }//if(prog==string.Empty)
+                        else
+                        {
+                            return wrd;
+                        }//else
+                    }//if(oper.Contains(prog.ElementAt(index)) || corch.Contains(prog.ElementAt(index))) 
+                    if (prog.ElementAt(index) != 32 && prog.ElementAt(index) != 9 && prog.ElementAt(index) != 0)
+                    { wrd += prog.ElementAt(index); }//aint blank space
+                    index += 1;
+                }//else
+            }//while
+            if (wrd != string.Empty)
+            { return wrd; }
+            else
+            {
+                return string.Empty;
+            }//else
+
+        }//read()
+        public string expressRead()
+        {
+            bool cnt = true;
+            string wrd = "";
+            do
+            {
+                wrd += prog.ElementAt(index);
+                if (prog.ElementAt(index) == 39 || prog.ElementAt(index) == 34)
+                {
+                    if (cnt) { cnt = false; }
+                    else
+                    {
+                        index += 1;
+                        return wrd;
+                    }//not first time its grabbed
+                }//if
+                index += 1;
+            } while (index < prog.Length);
+            return wrd;//llego al fin de la linea || tomar en cuenta si halla un ;
+        }//expressRead()
+
+        public void control()
+        {
+
+            string wrd = "";
+            while (index < prog.Length)
+            {
+                wrd = read();
+                if (wrd != string.Empty)
+                {
+                    token x = new token("nell", wrd, linea, index);
+                    if (keyword.Contains(wrd.ToLower()))
+                    {
+                        x = new token("keyword", wrd.ToLower(), linea, index);
+                    }//if(keyword.Contains(wrd))
+                    else
+                    {
+                        if (metodo.Contains(wrd.ToLower()))//esto lo metio el licea para saber si es una palabra reservada que obligatoriamente necesita parentesis despues
+                        {
+                            x = new token("metodo", wrd.ToLower(), linea, index);
+                        }
+                        else
+                        {
+                            if (oper.Contains(wrd))
+                            {
+                                x = new token("operator", wrd, linea, index);
+                            }//oper.contains wrd
+                            else
+                            {
+                                if (cmp.Contains(wrd))
+                                {
+                                    x = new token("comparison", wrd, linea, index);
+                                }//if comparison
+                                else
+                                {
+                                    if (corch.Contains(wrd))
+                                    {
+                                        x = new token("corchete", wrd, linea, index);
+                                    }//if special has it 
+                                    else
+                                    {
+                                        if (wrd.Length == 1 && wrd.ElementAt(0) == 44 || wrd.Length == 1 && wrd.ElementAt(0) == 59)
+                                        {
+                                            x = new token("punctuation", wrd, linea, index);
+                                        }//if wrd(wrd==44 || wrd==59)
+                                        else
+                                        {
+                                            double m = 0;
+                                            if (double.TryParse(wrd, out m))
+                                            {
+                                                x = new token("constant", wrd, linea, index);
+                                            }//if
+                                            else
+                                            {
+                                                if (wrd.ElementAt(0) >= 65 && wrd.ElementAt(0) <= 90 && onlyLetters(wrd) || wrd.ElementAt(0) >= 97 && wrd.ElementAt(0) <= 122 && onlyLetters(wrd))
+                                                {
+                                                    x = new token("identifier", wrd, linea, index);
+                                                }//if
+                                                else
+                                                {
+                                                    if (wrd.ElementAt(0) == 34 || wrd.ElementAt(0) == 39)
+                                                    {
+                                                        x = new token("string", wrd, linea, index);
+                                                    }//if(wrd.ElementAt(0)==34 || wrd.ElementAt(0)==39)
+                                                    else
+                                                    {
+                                                        x = new token("nell", wrd, linea, index);
+                                                        errors.AddLast("valor irreconocible:<" + wrd + "> en la linea: " + linea + "  posicion: " + index);
+                                                    }//else
+                                                }//else
+                                            }//else 
+                                        }//else
+                                    }//else not a metodo
+                                }//else
+                            }//else not a comparison
+                        }//else
+                    }//else
+
+                    if (x.tipo != "nell")
+                    {
+                        tokens.AddLast(x);
+                    }//if(x.tipo!="nell")
+
+                }//if(wrd!= string.Empty)
+
+            }//while index is smaller
+
+        }//LinkedLinst
+
+        public LinkedList<string> getErrors()
+        {
+            return errors;
+        }//getErrors
+        public LinkedList<token> getTokens()
+        {
+            return tokens;
+        }//getErrors
+        public void print()
+        {
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                Console.WriteLine("<" + tokens.ElementAt(i).tipo + "," + tokens.ElementAt(i).lexema + ">");
+            }//for(int i=0; i<tokens.Count; i++)
+            if (errors.Count > 0)
+            {
+                Console.WriteLine("los errores son:");
+            }
+            for (int i = 0; i < errors.Count; i++)
+            {
+                Console.WriteLine(errors.ElementAt(i));
+            }//for(int i=0; i<tokens.Count; i++)
+        }//print
+        public bool onlyLetters(string x, int c = 0)
+        {
+            if (c < x.Length)
+            {
+                if (x.ElementAt(c) >= 65 && x.ElementAt(c) <= 90 || x.ElementAt(c) >= 97 && x.ElementAt(c) <= 122 || x.ElementAt(c) >= 48 && x.ElementAt(c) <= 57)
+                {
+                    return onlyLetters(x, c + 1);
+                }//it's a letter
+                else
+                {
+                    return false;
+                }//not a letter
+            }//if(c<x.Length)
+            else
+            {
+                return true;
+            }//else
+        }//onlyLetters
+        public string getCompare()
+        {
+            string wrd = "" + prog.ElementAt(index);
+            if (index + 1 < prog.Length && prog.ElementAt(index + 1) == 61)
+            {
+                return wrd += prog.ElementAt(index + 1);
+            }//if(index+1< prog.Length&& prog.ElementAt(index+1)==61)
+            else
+            {
+                return wrd;
+            }//else
+        }//getCompare()
+    }//Lexico
+}//class Compilador
---
+++ Compilador/Sintactico.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador
+{
+    class Sintactico
+    {
+        Lexico lex;
+        LinkedList<token> tokens;
+        LinkedList<string> errores = new LinkedList<string>();
+        List<string> variables = new List<string>();
+        public Sintactico(Lexico lexico)
+        {
+            lex = lexico;
+            tokens = lex.getTokens();
+            validarCochineroLlaves();
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                getTipo(tokens.ElementAt(i), i);
+            }
+        }
+
+        public void getTipo(token tok, int pos)
+        {
+            switch (tok.tipo)
+            {
+                case "keyword":
+                    validarDeclaracionVariables(pos);
+                    break;
+                case "identifier":
+                    validarVariableExiste(tok.lexema, pos);
+                    break;
+            }
+        }
+        public void validarVariableExiste(string var, int pos)
+        {
+            if (!variables.Contains(var))
+            {
+                errores.AddLast("Linea: " + tokens.ElementAt(pos).linea + " Posicion: " + tokens.ElementAt(pos).index + Environment.NewLine + "Error: variable no declarada");
+            }
+        }
+
+        public void validarDeclaracionVariables(int pos)
+        {
+            List<string> tipo = new List<string>();
+            List<string> contenido = new List<string>();
+            int posAux = pos;
+            while (tokens.ElementAt(posAux).lexema != ";")
+            {
+                tipo.Add(tokens.ElementAt(posAux).tipo);
+                contenido.Add(tokens.ElementAt(posAux).lexema);
+                posAux++;
+            }
+            bool variableDeclaradaCorrectamente = true;
+            if (tokens.ElementAt(pos + 1).tipo != "identifier" && tokens.ElementAt(pos + 1).lexema != "[")
+            {
+                variableDeclaradaCorrectamente = false;
+                errores.AddLast("Linea: " + tokens.ElementAt(pos).linea + " Posicion: " + tokens.ElementAt(pos).index + Environment.NewLine + "Error: fallo al declarar una variable.");
+            }
+            if (tokens.ElementAt(pos).lexema == "palabra")
+            {
+                ;
+                if (tipo[tipo.IndexOf("identifier") + 1] != "corchete")
+                {
+                    variableDeclaradaCorrectamente = false;
+                    errores.AddLast("Linea: " + tokens.ElementAt(pos + 1).linea + " Posicion: " + (tipo.IndexOf("identifier") + 1) + Environment.NewLine + "Error: al declarar una variable string es necesario definir su tamaño.");
+                }
+            }
+            if (variableDeclaradaCorrectamente)
+            {
+                for (int i = 0; i < tipo.Count; i++)
+                {
+                    if (tipo[i] == "identifier")
+                    {
+                        variables.Add(contenido[i]);
+                    }
+                }
+            }
+        }
+
+        // aqui valido los tres tipos de corchete o lo que sea, los tres metodos son super redundantes es exactamente lo mismo solo que
+        // comparo diferentes caracteres, si se les ocurre algo mejor fierro
+        public void validarCochineroLlaves()
+        {
+            llaves();
+            corchetes();
+            parentesis();
+        }
+
+
+
+        public void llaves()
+        {
+            int parentesis = 0;
+            bool empiezaParentesisA = false;
+            bool hayParentesis = false;
+
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (tokens.ElementAt(i).lexema == "{")
+                {
+                    parentesis++;
+                    if (!hayParentesis)
+                        empiezaParentesisA = true;
+                    hayParentesis = true;
+                }
+                if (tokens.ElementAt(i).lexema == "}")
+                {
+                    parentesis--;
+                    hayParentesis = true;
+                }
+            }
+            if (parentesis < -1)
+            {
+                errores.AddLast(" Error: falta una llave de apertura {");
+            }
+            if (parentesis > 0)
+            {
+                errores.AddLast(" Error: falta llave de cierre }");
+            }
+            if (!empiezaParentesisA && hayParentesis)
+            {
+                errores.AddLast(" Error: debe comenzar con una llave de apertura {");
+            }
+        }
+
+        public void corchetes()
+        {
+            int parentesis = 0;
+            bool empiezaParentesisA = false;
+            bool hayParentesis = false;
+
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (tokens.ElementAt(i).lexema == "[")
+                {
+                    parentesis++;
+                    if (!hayParentesis)
+                        empiezaParentesisA = true;
+                    hayParentesis = true;
+                }
+                if (tokens.ElementAt(i).lexema == "]")
+                {
+                    parentesis--;
+                    hayParentesis = true;
+                }
+            }
+            if (parentesis < -1)
+            {
+                errores.AddLast(" Error: falta un corchete de apertura [");
+            }
+            if (parentesis > 0)
+            {
+                errores.AddLast(" Error: falta corchete de cierre ]");
+            }
+            if (!empiezaParentesisA && hayParentesis)
+            {
+                errores.AddLast(" Error: debe comenzar con un corchete de apertura [");
+            }
+        }
+
+        public void parentesis()
+        {
+            int parentesis = 0;
+            bool empiezaParentesisA = false;
+            bool hayParentesis = false;
+
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (tokens.ElementAt(i).lexema == "(")
+                {
+                    parentesis++;
+                    if (!hayParentesis)
+                        empiezaParentesisA = true;
+                    hayParentesis = true;
+                }
+                if (tokens.ElementAt(i).lexema == ")")
+                {
+                    parentesis--;
+                    hayParentesis = true;
+                }
+                if (tokens.ElementAt(i).tipo == "metodo" && tokens.ElementAt(i + 1).tipo != "corchete" && tokens.ElementAt(i).lexema != "sino")
+                {
+                    errores.AddLast(" Error: despues de la palabra reservada " + tokens.ElementAt(i).lexema + " va (");
+                }
+            }
+            if (parentesis < -1)
+            {
+                errores.AddLast(" Error: falta un parentesis de apertura (");
+            }
+            if (parentesis > 0)
+            {
+                errores.AddLast(" Error: falta parentesis de cierre )");
+            }
+            if (!empiezaParentesisA && hayParentesis)
+            {
+                errores.AddLast(" Error: debe comenzar con un parentesis de apertura (");
+            }
+        }
+
+        public void imprimir()
+        {
+            if (errores.Count > 0)
+            {
+                Console.WriteLine("los errores son:");
+            }
+            else
+            {
+                Console.WriteLine("programa compilado con exito!");
+            }
+            for (int i = 0; i < errores.Count; i++)
+            {
+                Console.WriteLine(errores.ElementAt(i));
+            }//for(int i=0; i<tokens.Count; i++)
+        }//print
+    }
+}
